fa1489881ab4766f1030c75a34bbd7f9e5d56562
Sagar_Jadhav <sagar.itpl@gmail.com>
2023-05-30 11:11:09 +0530

    loader impliment

--- src/component/CardUser.js
+++ src/component/CardUser.js
@@ -4,13 +4,23 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import fetch from "isomorphic-fetch";
+import Loader from "./Loader";
 function CardUser() {
   const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((response) => response.json())
+  //     .then((data) => setTodos(data));
+  // }, []);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
-      .then((data) => setTodos(data));
+      .then((data) => {
+        setTodos(data);
+        setIsLoading(false); // Set isLoading to false after data is fetched
+      });
   }, []);
 
   return (
@@ -25,24 +35,28 @@ function CardUser() {
         User App
       </h1>
       <Container style={{ background: "grey", paddingTop: "20px" }}>
-        <Row>
-          {todos?.map((todo) => (
-            <Col key={todo.id} xs={12} lg={4} xlg={4} md={6}>
-              <Card className="mb-3">
-                <Card.Body>
-                  <Card.Title>{todo.title}</Card.Title>
-                  <Card.Text>
-                    {todo.completed ? (
-                      <p style={{ color: "green" }}>Completed</p>
-                    ) : (
-                      <p style={{ color: "red" }}>Not completed</p>
-                    )}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Row>
+            {todos?.map((todo) => (
+              <Col key={todo.id} xs={12} lg={4} xlg={4} md={6}>
+                <Card className="mb-3">
+                  <Card.Body>
+                    <Card.Title>Title:- {todo.title}</Card.Title>
+                    <Card.Text>
+                      {todo.completed ? (
+                        <p style={{ color: "green" }}>Completed</p>
+                      ) : (
+                        <p style={{ color: "red" }}>Not completed</p>
+                      )}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        )}
       </Container>
     </>
   );
